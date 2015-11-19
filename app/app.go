7b74d72eacd852105c3c49eb63938b8c9827a81e
Javier Aguirre <contacto@javaguirre.net>
2015-11-19 19:30:56 +0100

    Listener file for backend

--- app/app.go
+++ app/app.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/asaskevich/EventBus"
-	"github.com/javaguirre/h4ckademy-workshop/app/lib"
-)
-
-// TODO Move listeners to a different file
-func newMessage(msg string) {
-	log.Println("new websocket message")
-	log.Println(msg)
-}
-
 func main() {
-
-	// TODO init listeners
-	bus := EventBus.New()
-	bus.Subscribe(lib.NEW_MESSAGE_EVENT, newMessage)
-
+	bus := initBus()
 	engine := initEngine(bus)
 	engine.Run(":3001")
 }
