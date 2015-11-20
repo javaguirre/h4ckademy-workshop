8e77aeb93bdfa0dbb9139f10005ddb2d932f5950
Javier Aguirre <contacto@javaguirre.net>
2015-11-20 11:26:17 +0100

    Subscriber renamed, added new subscribers

---
+++ app/subscriber.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+
+	"github.com/asaskevich/EventBus"
+	"github.com/javaguirre/h4ckademy-workshop/app/lib"
+)
+
+func persistMessage(message string) {
+	service := service.PersistService.New()
+	service.save(message)
+}
+
+func countMessage(message string) {
+	service := service.AnalyticsService.New()
+	service.update(message)
+}
+
+func logMessage(message string) {
+	log.Println("new websocket message")
+	log.Println(message)
+}
+
+func logNewDevice(device string) {
+	log.Println("new device arrived")
+	log.Println(device)
+}
+
+func initBus() (bus *EventBus.EventBus) {
+	bus = EventBus.New()
+	bus.Subscribe(lib.NEW_MESSAGE_EVENT, persistMessage)
+	bus.Subscribe(lib.NEW_MESSAGE_EVENT, countMessage)
+	bus.Subscribe(lib.NEW_MESSAGE_EVENT, logMessage)
+	bus.Subscribe(lib.NEW_DEVICE_ID, logNewDevice)
+
+	return
+}
